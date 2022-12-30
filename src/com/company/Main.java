58909c391d978b782ef9bf7af64101b0dad0b970
mferrara625 <mferrara401@yahoo.com>
2022-12-30 16:57:55 -0500

    Added Encapsulation and CLI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,18 @@ package com.company;
 import com.company.Abstraction.Puppy;
 import com.company.Classes.Class1;
 import com.company.Classes.Class2;
+import com.company.Encapsulation.Student;
 import com.company.Inheritance.Mouse1;
 import com.company.Inheritance.Mouse2;
 import com.company.Polymorphism.Bird;
 import com.company.Polymorphism.Robin;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void inheritanceDemo(){
+        System.out.println("INHERITANCE\n");
         Mouse1 m1 = new Mouse1();
         m1.connect();
         m1.scrollUp();
@@ -23,6 +27,7 @@ public class Main {
     }
 
     public static void polymorphismDemo(){
+        System.out.println("POLYMORPHISM\n");
         Bird bird = new Bird();
         bird.sing();
         Robin robin = new Robin();
@@ -30,6 +35,7 @@ public class Main {
     }
 
     public static void classDemo(){
+        System.out.println("CLASSES\n");
         Class1 c = new Class1();
         System.out.println(c.x);
         c.sayHi();
@@ -40,17 +46,43 @@ public class Main {
     }
 
     public static void abstractionDemo(){
+        System.out.println("ABSTRACT CLASSES\n");
         Puppy pup = new Puppy();
         pup.bark();
         pup.poop();
     }
 
+    public static void encapsulationDemo(){
+        System.out.println("ENCAPSULATION\n");
+        Student s = new Student();
+        s.setName("Mike");
+        s.setAge(21);
+
+        System.out.println(s.getName());
+        System.out.println(s.getAge());
+
+    }
+
     public static void main(String[] args) {
 
-        inheritanceDemo();
-        polymorphismDemo();
-        classDemo();
-        abstractionDemo();
-
+        Scanner scan = new Scanner(System.in);
+        while(true) {
+            System.out.println("Select from categories below to see a demonstration:\n" +
+                    "1. Inheritance\n" +
+                    "2. Polymorphism\n" +
+                    "3. Classes\n" +
+                    "4. Abstract Classes\n" +
+                    "5. Encapsulation");
+            int selection = scan.nextInt();
+            System.out.println("\n*\n");
+            switch (selection) {
+                case 1 -> inheritanceDemo();
+                case 2 -> polymorphismDemo();
+                case 3 -> classDemo();
+                case 4 -> abstractionDemo();
+                case 5 -> encapsulationDemo();
+            }
+            System.out.println("\n*\n");
+        }
     }
 }
